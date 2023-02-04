4839079c0e89b6e469930e5ad21d0f9c70197db9
LuciofsMelo <lucius.mat@gmail.com>
2023-02-04 15:07:52 -0300

    Aplicando Correções

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Aluno
 
-# Create your views here.
+def alunoView(request):
+    alunos = Aluno.objects.all()
+    return render(request, 'main/list.html', {'alunos':alunos})
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk = id)
+    return render(request, 'main/list.html', {'aluno':aluno})
