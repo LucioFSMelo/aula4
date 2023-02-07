26c0251f91bed42a5c400fbb843b2c483e9ac532
LuciofsMelo <lucius.mat@gmail.com>
2023-02-07 10:00:47 -0300

    segunda parte de edição

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+
+from main.forms import AlunoForm, redirect
 from .models import Aluno
 
 def alunoView(request):
@@ -8,3 +10,25 @@ def alunoView(request):
 def alunoIDview(request, id):
     aluno = get_object_or_404(Aluno, pk = id)
     return render(request, 'main/list.html', {'aluno':aluno})
+
+def exemplo(request):
+
+    if request.method == 'POST':
+        name = request.POST.get('nome', None)
+        email = request.POST.get('email', None)
+        telefone = request.POST.get('telefone', None)
+
+        print(name, email, telefone)
+    return render(request, 'main/indexx.html')
+
+def newAluno(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            aluno = form.save(commit=False)
+            aluno.user = request.user
+            aluno.save()
+            return redirect('/')
+    else:
+        form = AlunoForm()
+    return render(request, 'main/add_aluno.html', 'form':form)
